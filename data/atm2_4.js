334d801932a0554816a07dc2cbcf2295daa1ded9
Reymon Octavio <101917547+reymonoctavio@users.noreply.github.com>
2024-01-10 11:39:33 +0700

    Add files via upload

---
+++ data/atm2_4.js
@@ -0,0 +1,1 @@
+var json_atm2_4 = {"type":"FeatureCollection","name":"atm2_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Nama":"ATM Bank Nagari RSU Adnan WD","X":100.63834,"Y":-0.22293},"geometry":{"type":"Point","coordinates":[100.6383447,-0.22293193]}}]}
